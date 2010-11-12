d5663f7135aa6dfd019187a77f3ce6197f7245c5
lawand <lawand87@gmail.com>
2010-11-12 17:17:02 +0200

    created ReadErrorDialog class

--- gngt.pro
+++ gngt.pro
@@ -4,14 +4,17 @@ SOURCES += \
     noun.cpp \
     main.cpp \
     mainwindow.cpp \
-    editnoundialog.cpp
+    editnoundialog.cpp \
+    readerrordialog.cpp
 HEADERS += \
     noun.h \
     mainwindow.h \
-    editnoundialog.h
+    editnoundialog.h \
+    readerrordialog.h
 FORMS += \
     mainwindow.ui \
-    editnoundialog.ui
+    editnoundialog.ui \
+    readerrordialog.ui
 
 RESOURCES += \
     main.qrc
---
+++ readerrordialog.cpp
@@ -0,0 +1,40 @@
+/****************************************************************************
+**
+** Copyright 2010 Omar Lawand Dalatieh.
+** Contact: see the README file.
+**
+** This file is part of GNGT.
+**
+** GNGT is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** GNGT is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser Public License for more details.
+**
+** You should have received a copy of the GNU Lesser Public License
+** along with GNGT.  If not, see <http://www.gnu.org/licenses/>.
+**
+** For more information, check out the GNU General Public license found
+** in the COPYING file, the GNU Lesser Public license found in the
+** COPYING.LESSER file and the README file.
+**
+****************************************************************************/
+
+#include "readerrordialog.h"
+#include "ui_readerrordialog.h"
+
+ReadErrorDialog::ReadErrorDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::ReadErrorDialog)
+{
+    ui->setupUi(this);
+}
+
+ReadErrorDialog::~ReadErrorDialog()
+{
+    delete ui;
+}
