9a5fcc94bd4b45ada321fe1ebb796c1ab5ec4c10
lawand <lawand87@gmail.com>
2010-11-15 22:35:06 +0200

    changed version number displayed in the About dialog

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -286,7 +286,7 @@ void MainWindow::about()
 {
     QMessageBox::information(this, "About GNGT",
 
-                             "German Noun Gender Trainer (GNGT) v0.4 \nCreated "
+                             "German Noun Gender Trainer (GNGT) v0.5 \nCreated "
                              "by Omar Lawand Dalatieh <lawand87@gmail.com>\n\n"
 
                              "For help, check out the README file.\n\n"
