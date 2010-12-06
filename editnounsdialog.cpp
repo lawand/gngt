68dd6f9c62763e43620dbebc5c840774085c9874
lawand <lawand87@gmail.com>
2010-12-07 00:25:10 +0200

    made the dialog not change the layout if portrait mode is active

--- editnounsdialog.cpp
+++ editnounsdialog.cpp
@@ -32,6 +32,7 @@
 //implementation-specific data type(s)
 #include <QMessageBox>
 #include <QStringList>
+#include <QDesktopWidget>
 #include "editnoundialog.h"
 #include "noun.h"
 
@@ -122,37 +123,41 @@ EditNounsDialog::EditNounsDialog(QStringList* erroneousLines,
     ui->listWidget->setContextMenuPolicy(Qt::NoContextMenu);
 #endif
 
-    //chenge the layout in Maemo to make it horizontal
 #ifdef Q_WS_MAEMO_5
     //the done push button is not needed in maemo
     delete ui->donePushButton;
 
-    layout()->removeWidget(ui->addPushButton);
-    layout()->removeWidget(ui->editPushButton);
-    layout()->removeWidget(ui->removePushButton);
-    layout()->removeWidget(ui->removeAllPushButton);
+    //change the layout to make it horizontal
+    QRect screenGeometry = QApplication::desktop()->screenGeometry();
+    if (screenGeometry.width() > screenGeometry.height())
+    {
+        layout()->removeWidget(ui->addPushButton);
+        layout()->removeWidget(ui->editPushButton);
+        layout()->removeWidget(ui->removePushButton);
+        layout()->removeWidget(ui->removeAllPushButton);
 
-    QVBoxLayout *newRightLayout = new QVBoxLayout();
-    newRightLayout->addWidget(ui->addPushButton);
-    newRightLayout->addWidget(ui->editPushButton);
-    newRightLayout->addWidget(ui->removePushButton);
-    newRightLayout->addWidget(ui->removeAllPushButton);
+        QVBoxLayout *newRightLayout = new QVBoxLayout();
+        newRightLayout->addWidget(ui->addPushButton);
+        newRightLayout->addWidget(ui->editPushButton);
+        newRightLayout->addWidget(ui->removePushButton);
+        newRightLayout->addWidget(ui->removeAllPushButton);
 
-    layout()->removeWidget(ui->nounsFileLabel);
-    layout()->removeWidget(ui->listWidget);
-    layout()->removeWidget(ui->numberOfNounsLabel);
+        layout()->removeWidget(ui->nounsFileLabel);
+        layout()->removeWidget(ui->listWidget);
+        layout()->removeWidget(ui->numberOfNounsLabel);
 
-    QVBoxLayout *newLeftLayout = new QVBoxLayout();
-    newLeftLayout->addWidget(ui->nounsFileLabel);
-    newLeftLayout->addWidget(ui->listWidget);
-    newLeftLayout->addWidget(ui->numberOfNounsLabel);
+        QVBoxLayout *newLeftLayout = new QVBoxLayout();
+        newLeftLayout->addWidget(ui->nounsFileLabel);
+        newLeftLayout->addWidget(ui->listWidget);
+        newLeftLayout->addWidget(ui->numberOfNounsLabel);
 
-    QHBoxLayout *newMainLayout = new QHBoxLayout();
-    newMainLayout->addLayout(newLeftLayout);
-    newMainLayout->addLayout(newRightLayout);
+        QHBoxLayout *newMainLayout = new QHBoxLayout();
+        newMainLayout->addLayout(newLeftLayout);
+        newMainLayout->addLayout(newRightLayout);
 
-    delete this->layout();
-    this->setLayout(newMainLayout);
+        delete this->layout();
+        this->setLayout(newMainLayout);
+    }
 #endif
 }
 
